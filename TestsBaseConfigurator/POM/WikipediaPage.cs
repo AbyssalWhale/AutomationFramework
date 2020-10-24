fbf5cead7e12c605442f468f9a5749a10572b15b
Oleg Gaevoy <o.gaevoy@icloud.com>
2020-10-25 01:50:42 +0200

    adding wiki page test

---
+++ TestsBaseConfigurator/POM/WikipediaPage.cs
@@ -0,0 +1,32 @@
+﻿using AutomationFramework.Entities;
+using NUnit.Framework;
+using OpenQA.Selenium;
+
+namespace TestsBaseConfigurator.POM
+{
+    public class WikipediaPage : BasePagePOM
+    {
+        public override string Title => "Wikipedia";
+
+        private By searchInput => By.XPath("//input[@id='searchInput']");
+
+        public WikipediaPage(WebDriverManager webDriverManager, RunSettingManager runSettingsManager, LogManager logManager, FolderManager folderManager) 
+            : base(webDriverManager, runSettingsManager, logManager, folderManager)
+        {
+            Assert.IsTrue(IsAt(), $"It's expected page be: {Title} but was: {_webDriverManager.GetPageTitle()}");
+        }
+
+        protected override bool IsAt()
+        {
+            _webDriverManager.IsPageLoaded();
+            return _webDriverManager.GetPageTitle().Equals(Title);
+        }
+
+        public void FindAndOpenArticle(string articleName)
+        {
+            _webDriverManager.SendKeys(searchInput, articleName);
+            _webDriverManager.IsPageLoaded();
+            _webDriverManager.ClickOnElement(By.XPath($"//div[@id='typeahead-suggestions']//*[text()='{articleName}']"));
+        }
+    }
+}
