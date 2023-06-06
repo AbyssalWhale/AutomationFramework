287b9f649e45c1b7f1a996b97224d284e6d7ddf2
AbyssalWhale <53709071+AbyssalWhale@users.noreply.github.com>
2023-06-06 13:58:07 +0200

    up

--- RegressionTests/UI/HomePage.cs
+++ RegressionTests/UI/HomePage.cs
@@ -14,14 +14,23 @@ namespace UI
             //Arrange
             var platform = "PlayStation";
             Assert.IsTrue(HomePage.IsLoaded(), UIAMessages.PageNotLoaded(HomePage.Title));
-            var gamePlatforms_API = Controllers.Platforms.Get_VideoGamesPlatform(platform, strictEqual: false).Result;
+            var platform_API = Controllers.Platforms.Get_VideoGamesPlatform(platform, strictEqual: false).Result;
 
             //Act
             HomePage.GamesGrid.Platforms
                 .Click_Platform_DropDown()
                 .Click_Platform_Option(platform);
             HomePage.GamesGrid.IsLoaded();
+            var card_Titles_UI = HomePage.GamesGrid.Get_Cards_Titles();
 
+            //Assert.Multiple(() =>
+            //{
+            //    foreach (var game_UI in card_Titles_UI)
+            //    {
+            //        var isFound = platform_API.Any(p => p.games.Any(g => g.name.ToLower().Equals(game_UI.ToLower())));
+            //        Assert.IsTrue(isFound, $"Expected that UI game is found in API platform data after filtering UI by that platform. \n Game: {game_UI}");
+            //    }
+            //});
 
             //Assert
             //Assert.IsTrue(HomePage.Is_Seller_ContainerSection_Displayed, UIAMessages.ElementIsNotDisplayed(nameof(HomePage.Is_Seller_ContainerSection_Displayed), HomePage.Title));
