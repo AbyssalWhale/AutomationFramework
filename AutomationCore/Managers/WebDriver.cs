c0fd42e1a751070a01d4a9abde450a3a67a9bec4
AbyssalWhale <53709071+AbyssalWhale@users.noreply.github.com>
2023-06-08 17:30:03 +0200

    disable Parallelizable

--- AutomationCore/Managers/WebDriver.cs
+++ AutomationCore/Managers/WebDriver.cs
@@ -59,19 +59,12 @@ namespace AutomationCore.Managers
 
         private ChromeOptions SetChrome()
         {
-            //System.Environment.SetEnvironmentVariable("webdriver.chrome.whitelistedIps", "");
             ChromeOptions options = new ChromeOptions();
             options.AddArgument("--start-maximized");
-            //options.AddArgument("--whitelisted-ips='*'");
-            //options.AddArgument("--allowed-ips='*'");
-            //options.AddArgument("--allowed-origins='*'");
-            //options.AddAdditionalChromeOption("allowed-ips", "*");
-            //options.AddAdditionalChromeOption("allowed-origins", "*");
 
             if (_runSettings.Headless)
             {
                 options.AddArgument("--headless=new");
-                //options.AddArgument("--headless");
             };
 
             return options;
